2605b4a7aae46641f7fda0c37e1e64d5644e2cb3
yezekieru10 <yezekieru@gmail.com>
2026-01-31 23:18:05 +0800

    test fn

---
+++ Zeyn/Testzeyn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Testzeyn() {
+ 
+
+  return (
+    <div className="container_zeyn" style={{ backgroundColor: "#cb0f0f" }}>
+      <h1>Test Zeyn</h1>
+    </div>
+  );
+}
+
+export default Testzeyn;
