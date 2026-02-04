26d1da0eb02758ffc8938aa5753906b5055032f4
PC_Kontrol_Room <sarani090500@gmail.com>
2026-02-04 13:28:41 +0800

    landing security

---
+++ Rizki/TestIki.jsx
@@ -0,0 +1,12 @@
+import React, { useRef } from "react";
+
+function Testiki() {
+  
+  return (
+    <div className="container-iki">
+     <h1>TESTTTTTT</h1>
+    </div>
+  );
+}
+
+export default Testiki;
